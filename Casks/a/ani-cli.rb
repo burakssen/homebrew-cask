c5c0262a54cf9c9505106ffffdff7dc8a0ba3dfc
burak şen <buraksen7@hotmail.com>
2024-11-04 17:20:25 +0100

    Fix

--- Casks/a/ani-cli.rb
+++ Casks/a/ani-cli.rb
@@ -2,7 +2,7 @@ cask "ani-cli" do
     version :latest
     sha256 :no_check
   
-    url "https://github.com/pystardust/ani-cli/releases/latest/download/ani-cli.zip"
+    url "https://github.com/pystardust/ani-cli/releases/latest/download/ani-cli"
     name "ani-cli"
     desc "A command-line interface for anime"
     homepage "https://github.com/pystardust/ani-cli"
