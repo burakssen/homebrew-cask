520eb4a98710167641bf291f54b4e7f747ee98af
burak şen <buraksen7@hotmail.com>
2024-11-04 17:33:56 +0100

    Add surfshark

---
+++ Casks/s/surfshark.rb
@@ -0,0 +1,18 @@
+case "surfshark" do
+    version :latest
+    sha256 :no_check
+
+    url "https://downloads.surfshark.com/macOS/latest/Surfshark.dmg"
+    name "Surfshark"
+    desc "Effortless online privacy with a VPN."
+    homepage "https://surfshark.com"
+    app "Surfshark.app"
+
+    uninstall quit: "com.surfshark.Surfshark"
+    zap trash: [
+        "~/Library/Application Support/Surfshark",
+        "~/Library/Preferences/com.surfshark.Surfshark.plist",
+    ]
+end
+
+
