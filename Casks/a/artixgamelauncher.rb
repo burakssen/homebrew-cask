de6f680652e35dc830f8a285d4da08f97f25c8b3
burak şen <buraksen7@hotmail.com>
2024-11-04 16:50:55 +0100

    add artix game launcher

---
+++ Casks/a/artixgamelauncher.rb
@@ -0,0 +1,16 @@
+cask "artix-game-launcher" do
+    version "2.1.2"
+    sha256 "bec7a75bba4e11630233a7e66ad5af6d6bf82b9efde77189363d570fe75f59d0"
+
+    url "https://launch.artix.com/latest/Artix%20Game%20Launcher.dmg"
+    name "Artix Game Launcher"
+    desc "One app. All your favorite Artix games."
+    homepage "https://www.artix.com"
+
+    app "Artix Game Launcher.app"
+
+    zap trash: [
+        "~/Library/Application Support/Artix Game Launcher",
+        "~/Library/Preferences/com.burakssen.artixgamelauncher.plist"
+    ]
+end
